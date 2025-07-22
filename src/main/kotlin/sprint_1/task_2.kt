a470ab693ee51f0aff6cc7504bf1c5aadbe76fac
dellevar-wow <lenkolis10@gmail.com>
2025-07-22 23:14:17 +0700

    KS-1-2 complete task

---
+++ src/main/kotlin/sprint_1/task_2.kt
@@ -0,0 +1,13 @@
+package org.example.sprint_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksMessage: String = "Спасибо за покупку!"
+    println("Количество заказов: $numberOfOrders. $thanksMessage")
+
+    var numberOfEmployees = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+    println("Количество работников: $numberOfEmployees")
+}
