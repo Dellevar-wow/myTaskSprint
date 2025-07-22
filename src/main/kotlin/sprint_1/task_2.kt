61be5303b59313e215ae81b9ad2fada3a5ef0bac
dellevar-wow <lenkolis10@gmail.com>
2025-07-22 21:54:59 +0700

    task2 add new var

---
+++ src/main/kotlin/sprint_1/task_2.kt
@@ -0,0 +1,13 @@
+package org.example.sprint_1
+
+fun main() {
+    var numberOfOrders: Int = 100
+    val thanksMessage: String = "Спасибо за покупку!"
+    println("Количество заказов: $numberOfOrders. $thanksMessage")
+
+    var numberOfEmployees = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees -= 1
+    println("Количество работников: $numberOfEmployees")
+}
