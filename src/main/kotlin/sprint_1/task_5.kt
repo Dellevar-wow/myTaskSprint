92b053958ea4dc40c8d81311630a13e1a600caa2
dellevar-wow <lenkolis10@gmail.com>
2025-07-23 18:57:25 +0700

    KS-1-5 correct error

--- src/main/kotlin/sprint_1/task_5.kt
+++ src/main/kotlin/sprint_1/task_5.kt
@@ -1,11 +1,12 @@
 package org.example.sprint_1
 
-import kotlin.math.round
+import kotlin.math.pow
 
+const val SECONDS_IN_MINUTES = 60.0
 fun main() {
     var seconds = 6480
-    val hours= seconds/3600
-    val minutes = seconds % 3600 /60
-    seconds = seconds % 3600 % 60
-    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+    val hours = seconds / SECONDS_IN_MINUTES.pow(2).toInt()
+    val minutes = seconds % SECONDS_IN_MINUTES.pow(2).toInt() / SECONDS_IN_MINUTES.toInt()
+    seconds = seconds % SECONDS_IN_MINUTES.pow(2).toInt() % SECONDS_IN_MINUTES.toInt()
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
