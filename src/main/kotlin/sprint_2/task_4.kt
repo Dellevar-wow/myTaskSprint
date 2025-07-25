370aee97749ae1902897e650a2c1c919b5bfa2ce
lenko <lenkolis10@gmail.com>
2025-07-25 15:06:33 +0700

    KS-2-3 correction

--- src/main/kotlin/sprint_2/task_4.kt
+++ src/main/kotlin/sprint_2/task_4.kt
@@ -1,12 +1,12 @@
 package org.example.sprint_2
 
 fun main() {
-    val abilityBuff = 0.2
+    val abilityBuff = 20
     val nCrystalOreWithoutBuff = 7
     val nIronOreWithoutBuff = 11
 
-    val nCrystalOreWithBuff = (nCrystalOreWithoutBuff * abilityBuff).toInt()
-    val nIronOreWithBuff = (nIronOreWithoutBuff * abilityBuff).toInt()
+    val nCrystalOreWithBuff = (nCrystalOreWithoutBuff * abilityBuff/100).toInt()
+    val nIronOreWithBuff = (nIronOreWithoutBuff * abilityBuff/100).toInt()
 
     println("Дополнительная кристалическая руда: $nCrystalOreWithBuff")
     println("Дополнительная железная руда: $nIronOreWithBuff")
