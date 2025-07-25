b48c5252dfa501998c055eff0c3b0c7f7bbda99f
lenko <lenkolis10@gmail.com>
2025-07-25 14:49:38 +0700

    KS-2-1 correction

--- src/main/kotlin/sprint_2/task_1.kt
+++ src/main/kotlin/sprint_2/task_1.kt
@@ -2,9 +2,8 @@ package org.example.sprint_2
 
 fun main() {
     val marksEnglish = listOf(3,4,3,5)
-    val numberOfHuman = marksEnglish.count()
 
-    val averageMark = marksEnglish.sum().toDouble() / numberOfHuman
+    val averageMark = marksEnglish.sum().toDouble() / marksEnglish.count()
 
     print(averageMark)
 }
