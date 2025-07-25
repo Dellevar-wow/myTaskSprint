348ce174ecde5b7fb9c8d9daf2da1463d91498cd
lenko <lenkolis10@gmail.com>
2025-07-25 14:48:33 +0700

    KS-2-5 correction

--- src/main/kotlin/sprint_2/task_5.kt
+++ src/main/kotlin/sprint_2/task_5.kt
@@ -4,10 +4,10 @@ import kotlin.math.pow
 
 fun main() {
     val nMoney = 70000
-    val interestRate = 16.7/100
+    val interestRate = 16.7
     val term = 20
 
-    val depositThrowTerm = nMoney * (1 + interestRate).pow(term)
+    val depositThrowTerm = nMoney * (1 + interestRate/100).pow(term)
 
     println("%.3f".format(depositThrowTerm))
 }
