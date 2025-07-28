357dad9a060ebec98625f8463cdf444bff453e10
lenko <lenkolis10@gmail.com>
2025-07-28 15:14:29 +0700

    KS-3-2 complete

---
+++ src/main/kotlin/sprint_3/task_2.kt
@@ -0,0 +1,14 @@
+package org.example.sprint_3
+
+fun main() {
+    var sername = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Серегеевна"
+    var age = 20
+
+    println("ФИО: $sername $name $patronymic, возраст: $age")
+
+    sername = "Сидорова"
+    age = 22
+    println("ФИО: $sername $name $patronymic, возраст: $age")
+}
