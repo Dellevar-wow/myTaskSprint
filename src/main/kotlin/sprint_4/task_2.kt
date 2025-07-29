d289473943a421f77c018fda50a175794890ba40
lenko <lenkolis10@gmail.com>
2025-07-29 14:36:54 +0700

    KS-4-2 complete

--- src/main/kotlin/sprint_4/task_2.kt
+++ src/main/kotlin/sprint_4/task_2.kt
@@ -7,10 +7,14 @@ fun main() {
     val weightOfCargo2 = 50
     val cargoVolume2 = 100
 
-    println("Груз с весом $weightOfCargo1 кг и объемом $cargoVolume1" +
-            " л соответствует категории 'Average': " +
-            "${weightOfCargo1 > 35 && weightOfCargo1<= 100 && cargoVolume1 < 100}")
-    println("Груз с весом $weightOfCargo2 кг и объемом $cargoVolume2" +
-            " л соответствует категории 'Average': " +
-            "${weightOfCargo2 > 35 && weightOfCargo2<= 100 && cargoVolume2 < 100}")
+    println(
+        "Груз с весом $weightOfCargo1 кг и объемом $cargoVolume1" +
+                " л соответствует категории 'Average': " +
+                "${weightOfCargo1 > 35 && weightOfCargo1 <= 100 && cargoVolume1 < 100}"
+    )
+    println(
+        "Груз с весом $weightOfCargo2 кг и объемом $cargoVolume2" +
+                " л соответствует категории 'Average': " +
+                "${weightOfCargo2 > 35 && weightOfCargo2 <= 100 && cargoVolume2 < 100}"
+    )
 }
