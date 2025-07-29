9f54687dd2850731a3a8bcc2245d6dc4dbaa3da7
lenko <lenkolis10@gmail.com>
2025-07-29 15:48:45 +0700

    KS-4-4 complete

---
+++ src/main/kotlin/sprint_4/task_4.kt
@@ -0,0 +1,10 @@
+package org.example.sprint_4
+
+fun main() {
+    val day = 5
+
+    println("Упражнения для рук: ${day == 5}")
+    println("Упражнения для ног: ${day != 5}")
+    println("Упражнения для спины: ${day != 5}")
+    println("Упражнения для пресса: ${day == 5}")
+}
