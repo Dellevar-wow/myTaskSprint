109d0a8f948ff39b1f13d1a69a7b5abfcf07b435
lenko <lenkolis10@gmail.com>
2025-07-30 17:57:39 +0700

    KS-3-5 correcting

--- src/main/kotlin/sprint_3/task_5.kt
+++ src/main/kotlin/sprint_3/task_5.kt
@@ -3,9 +3,7 @@ package org.example.sprint_3
 fun main() {
     val move = "D2-D4;0"
 
-    val whereFrom = move.split("[-;]".toRegex())[0]
-    var where = move.split("[-;]".toRegex())[1]
-    var moveNumber = move.split("[-;]".toRegex())[2]
+    val (whereFrom, where, moveNumber) = move.split("-", ";")
 
     println("Откуда: $whereFrom")
     println("Куда: $where")
